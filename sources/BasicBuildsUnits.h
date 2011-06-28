3372fd4e5a6c0195995afe21c5bdf897be82f39a
Setes-fb <setes.mi@googlemail.com>
2011-06-28 22:25:12 +0200

    Start with the units

---
+++ sources/BasicBuildsUnits.h
@@ -0,0 +1,17 @@
+/****************************************************************************/
+/* CBasicBuildsUnits class - base class for builds and units                */
+/*                                                                          */
+/* author : Florian Blasius (Setes-fb) - June 28th 2011                     */
+/*                                                                          */
+/****************************************************************************/
+
+#ifndef BASICBUILDSUNITS_H
+#define BASICBUILDSUNITS_H
+
+#include "MenuElements.h"
+
+class CBasicBuildsUnits : public CMenuElements{
+
+}// class CBasicBuldsUnits
+
+#endif
